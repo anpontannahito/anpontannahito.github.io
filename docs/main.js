1e04048b1b4d187d4b468e90123cda74cd954ba7
baeloon <91828014+anpontannahito@users.noreply.github.com>
2026-01-20 14:17:03 +0900

    非同期読み込みの削除

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,4 @@
-async function loadHTML(id, file) {
+function loadHTML(id, file) {
     const element = document.getElementById(id);
     const response = await fetch(file);
     const html = await response.text();
