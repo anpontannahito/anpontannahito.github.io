9e5052ba80dd4536e21abaca2783e96e05f04301
anpontannahito <tabetataytomato@gmail.com>
2026-02-02 23:56:23 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,13 @@
-function loadHTML(id, file) {
+async function loadHTML(id, file) {
     const element = document.getElementById(id);
     const response = await fetch(file);
     const html = await response.text();
     element.innerHTML = html;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    loadHTML("header", "/assets/header.html");
-    loadHTML("footer", "/assets/footer.html");
+document.addEventListener("DOMContentLoaded", async () => {
+    document.body.style.visibility = "hidden";
+    await loadHTML("header", "/assets/header.html");
+    await loadHTML("footer", "/assets/footer.html");
+    document.body.style.visibility = "visible";
 });
